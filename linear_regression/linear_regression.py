c2589d32778e0aacd01c0439d3a5e1c417af540a
root <natrofl@gmail.com>
2021-08-27 14:12:05 +0300

    fix linear r2

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -3,20 +3,14 @@ import json
 import argparse
 from pathlib import Path
 
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
 def _linear_regression(args):
 
-    # Open and reads file "data"
     with open(args.data) as data_file:
         data = json.load(data_file)
     
-    # The excted data type is 'dict', however since the file
-    # was loaded as a json object, it is first loaded as a string
-    # thus we need to load again from such string in order to get 
-    # the dict-type object.
     data = json.loads(data)
 
     x_train = data['x_train']
@@ -24,32 +18,25 @@ def _linear_regression(args):
     x_test = data['x_test']
     y_test = data['y_test']
     
-    # Initialize and train the model
     model = LinearRegression()
     model.fit(x_train, y_train)
 
-    # Get predictions
     y_pred = model.predict(x_test)
     
-    # Get accuracy
-    accuracy = accuracy_score(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
-    # Save output into file
-    with open(args.accuracy, 'w') as accuracy_file:
-        accuracy_file.write(str(accuracy))
-
+    with open(args.r2, 'w') as r2_file:
+        r2_file.write(str(r2))
 
 
 if __name__ == '__main__':
 
-    # Defining and parsing the command-line arguments
     parser = argparse.ArgumentParser(description='My program description')
     parser.add_argument('--data', type=str)
-    parser.add_argument('--accuracy', type=str)
+    parser.add_argument('--r2', type=str)
 
     args = parser.parse_args()
 
-    # Creating the directory where the output file will be created (the directory may or may not exist).
-    Path(args.accuracy).parent.mkdir(parents=True, exist_ok=True)
+    Path(args.r2).parent.mkdir(parents=True, exist_ok=True)
     
     _linear_regression(args)
